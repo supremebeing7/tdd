73a5fefd97052521b24cb511f6a3a2479579d9e2
Mark Lehman <markopolo@gmail.com>
2014-05-29 00:26:25 -0700

    pass all tests

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,20 @@
 class Person < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :comapny_name, :email
-  
+  attr_accessible :first_name, :last_name, :company_name, :email
+
   has_many :notes
+
+  def self.merge(winner, loser)
+    winner.email = loser.email if winner.email == ""
+    winner.notes << loser.notes
+    loser.destroy
+    winner
+  end
+
+  def duplicates?(person)
+    if person
+      self.first_name == person.first_name &&
+      self.last_name == person.last_name &&
+      self.email == person.email
+    end
+  end
 end
